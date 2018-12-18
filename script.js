a3ae9a4e1e17deed7b16ff9562d6de75bcbf45a2
Unknown <dexterm352@gmail.com>
2018-12-18 14:00:30 +0100

    Html, Css & js Update
    Adding some more divs to Html.
    Centering everything in Css.
    Adding functions to buttons in js.

--- script.js
+++ script.js
@@ -6,3 +6,43 @@ function myFunction() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById("colorChange").style.width = scrolled + "%";
 }
+
+function clickButton() {
+    $(document).ready(function() {
+        $("button.button1").click(function() {
+            $("img.prog1").fadeOut(1000)
+        })
+    })
+
+    $(document).ready(function() {
+        $("button.button2").click(function() {
+            $("img.prog1").fadeIn(1000)
+        })
+    })
+
+    $(document).ready(function() {
+        $("button.button3").click(function() {
+            $("img.prog2").fadeOut(1000)
+        })
+    })
+
+    $(document).ready(function() {
+        $("button.button4").click(function() {
+            $("img.prog2").fadeIn(1000)
+        })
+    })
+
+    var url = "https://api.nasa.gov/planetary/apod?api_key=A35ovPJhsWs7dgeoF34jlToPvuZf0mhYJpyWE4IM"
+    $("#nasa").hide()
+
+    $("#space").click(function() {
+        $.get(url, function(data) {
+            $("#space").fadeOut()
+            $("#nasa").attr("src", data.url).fadeIn()
+            $("#head").text(data.title)
+            $("#description").text(data.explanation)
+        })
+    })
+}
+
+$(document).ready(clickButton)
