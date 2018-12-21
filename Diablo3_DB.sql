d141d3f2ec643b33372fd223b36a388ab4091e50
Unknown <dexterm352@gmail.com>
2018-12-21 09:36:51 +0100

    Database update

--- Diablo3_DB.sql
+++ Diablo3_DB.sql
@@ -1,4 +1,4 @@
-/*CREATE TABLE season12h
+/*CREATE TABLE season10h
 (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	Rank INT NOT NULL,
@@ -9,7 +9,7 @@
 	modified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE season12s
+CREATE TABLE season10s
 (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	Rank INT NOT NULL,
@@ -20,46 +20,46 @@ CREATE TABLE season12s
 	modified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );*/
 
-INSERT INTO season12h(Rank, Hero, Class, GRIFT_SOLO) VALUES
-(1, "Ouroboros", "Barbarian", 112),
-(2, "Ouroboros", "Crusader", 112),
-(3, "Todos", "Necromancer", 110),
-(4, "Cav", "Barbarian", 110),
-(5, "Cuthalion", "Wizard", 109),
-(6, "Elyzael", "Crusader", 109),
-(7, "Lazyncrazy", "Wizard", 109),
-(8, "Magangsin", "Wizard", 109),
-(9, "Fiammiferina", "Wizard", 108),
-(10, "Nihlatak", "Crusader", 108),
-(11, "Ripperi", "Crusader", 108),
-(12, "Rik", "Wizard", 108),
-(13, "Pewpew", "Necromancer", 108),
-(14, "Pphill", "Monk", 108),
-(15, "Vapo", "Crusader", 108),
-(16, "Chencliff", "Crusader", 108),
-(17, "Hu5ky", "Crusader", 107),
-(18, "Deconina", "Wizard", 107),
-(19, "Halogseonjang", "Crusader", 107),
-(20, "Cyphol", "Barbarian", 107);
+INSERT INTO season10h(Rank, Hero, Class, GRIFT_SOLO) VALUES
+(1, "Aradina", "Wizard", 100),
+(2, "Trixxa", "Wizard", 99),
+(3, "Kylesoska", "Wizard", 96),
+(4, "Slagk", "Crusader", 96),
+(5, "Death", "Wizard", 95),
+(6, "Kennyorange", "Wizard", 95),
+(7, "Voxmaster", "Demon Hunter", 94),
+(8, "Suna", "Demon Hunter", 94),
+(9, "Cyalithe", "Wizard", 93),
+(10, "Maverick", "Wizard", 93),
+(11, "Pyroman43", "Crusader", 93),
+(12, "Bigdog", "Crusader", 92),
+(13, "Gim-yujeong", "Demon Hunter", 92),
+(14, "Storm", "Demon Hunter", 92),
+(15, "Tabuu", "Demon Hunter", 92),
+(16, "Kennyorange", "Demon Hunter", 91),
+(17, "Ryanfuckinj", "Wizard", 91),
+(18, "Rogerclee", "Crusader", 91),
+(19, "Dandro", "Monk", 91),
+(20, "Burninator", "Wizard", 91);
 
-INSERT INTO season12s(Rank, Hero, Class, GRIFT_SOLO) VALUES
-(1, "Residuals", "Crusader", 125),
-(2, "Cornbread", "Crusader", 124),
-(3, "Psycho", "Crusader", 123),
-(4, "Lumines", "Crusader", 123),
-(5, "Duder", "Crusader", 123),
-(6, "Julien", "Wizard", 122),
-(7, "Azyf", "Wizard", 122),
-(8, "Gangjiyeong", "Crusader", 121),
-(9, "Lame", "Crusader", 121),
-(10, "Ru yåo zhi måli", "Crusader", 121),
-(11, "Quick", "Barbarian", 121),
-(12, "Quick", "Crusader", 121),
-(13, "Flateric", "Crusader", 120),
-(14, "Vaccaria", "Wizard", 120),
-(15, "Mrangryg", "Crusader", 120),
-(16, "Peter", "Crusader", 120),
-(17, "Jklee", "Crusader", 120),
-(18, "Wstsitm58048", "Necromancer", 120),
-(19, "Zyrram", "Crusader", 120),
-(20, "Kelthor", "Crusader", 120);
+INSERT INTO season10s(Rank, Hero, Class, GRIFT_SOLO) VALUES
+(1, "Turgor", "Wizard", 111),
+(2, "Switched", "Crusader", 108),
+(3, "Painshock", "Wizard", 107),
+(4, "Brbc", "Monk", 107),
+(5, "Unknown", "Wizard", 107),
+(6, "Nakiin", "Wizard", 106),
+(7, "Kawaiitryn", "Monk", 106),
+(8, "Iht", "Wizard", 105),
+(9, "Ddog", "Wizard", 105),
+(10, "Ospescador", "Wizard", 105),
+(11, "Primero", "Wizard", 105),
+(12, "Magun", "Wizard", 105),
+(13, "Earth", "Wizard", 105),
+(14, "Ajjic", "Wizard", 105),
+(15, "Llnemesisll", "Witch Doctor", 104),
+(16, "Alkolgun", "Demon Hunter", 104),
+(17, "Gimhodong", "Wizard", 104),
+(18, "Ajjic", "Wizard", 104),
+(19, "Duongvecoem", "Witch Doctor", 104),
+(20, "Nalchitalig", "Wizard", 104);
